f5b40092a0a6f14208c849a2fe6249930eee0165
ernesthenry <henry38ernest@gmail.com>
2022-06-25 23:32:36 +0300

    Raw queries

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
     //
-    public function index(){
-        return view('welcome');
+    public function index()
+    {
+
+        // raw queries
+        $users =  DB::select('select * from users');
+        // Query Builder
+        $users = DB::table('users')->where(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
     }
 }
