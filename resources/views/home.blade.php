9feeb6690cb6b9815a94ec9769e4b709c1229b71
ernesthenry <henry38ernest@gmail.com>
2022-06-25 22:41:12 +0300

    Fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,15 @@
-@extends('layouts.base')
+<x-base>
 
-@section('name', "Ernest")
-@section('content')
-<h1 class="text-4xl font-bold">Home</h1>
-<p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque in saepe ea eius ex recusandae impedit earum! Aperiam quibusdam quos voluptate quisquam cum saepe incidunt odio delectus! Eius, obcaecati asperiores.</p>
-@endsection
+    <x-slot name="name">
+        Ernest
+    </x-slot> 
+
+    <x-slot name="content">
+        <h1 class="text-4xl font-bold">Home</h1>
+        <p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque in saepe ea eius ex
+            recusandae impedit earum! Aperiam quibusdam quos voluptate quisquam cum saepe incidunt odio delectus! Eius,
+            obcaecati asperiores.</p>
+    </x-slot>
+        
+
+</x-base>
